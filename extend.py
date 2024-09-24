90f56d36c5e4dff64cb3ed4fbddc09bfa2fab165
amirali <amirali@DESKTOP-2P0IDPU.localdomain>
2024-09-24 19:59:54 -0400

    Making extension and script generator compatible with hw3 experiment

--- extend.py
+++ extend.py
@@ -1,21 +1,22 @@
 # on my machine, i ran this with:  
 #   python3.13 -B extend.py ../moot/optimize/[comp]*/*.csv
 
-import random
-from ezr import the, DATA, csv, dot, chebyshevs
+import random, sys
+from ezr import the, DATA, csv
 import stats
-from math import exp
-import time
 
-d = DATA().adds(csv("data/optimize/misc/auto93.csv"))
+dataset = sys.argv[1]
+
+d = DATA().adds(csv(dataset))
 b4 = [d.chebyshev(row) for row in d.rows]
-low_dimensions_somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
-high_dimensions_somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
+low_dimensions_somes = []
+high_dimensions_somes = []
+(low_dimensions_somes if len(d.cols.x) < 6 else high_dimensions_somes).append(stats.SOME(b4,f"asIs,{len(d.rows)}"))
 
-repeats = 20
+repeats = 1
 for N in (20,30,40,50):
   the.Last = N
-  d = DATA().adds(csv("data/optimize/misc/auto93.csv"))
+  d = DATA().adds(csv(dataset))
   dumb = [d.clone(random.choices(d.rows, k=N)).chebyshevs().rows for _ in range(repeats)]
   dumb = [d.chebyshev( lst[0] ) for lst in dumb]
 
@@ -26,11 +27,10 @@ for N in (20,30,40,50):
   smart = [d.chebyshev( lst[0] ) for lst in smart]
   (low_dimensions_somes if len(d.cols.x) else high_dimensions_somes).append(stats.SOME(smart,f"smart,{N}"))
 
-stats.report(low_dimensions_somes, 0.01)
-print('\n\n')
-stats.report(high_dimensions_somes, 0.01)
+(stats.report(low_dimensions_somes, 0.01) if len(d.cols.x) < 6 else stats.report(high_dimensions_somes, 0.01))
 
 #-------------------------------------------------------------------------------
+
 '''
 d = DATA().adds(csv("data/optimize/misc/auto93.csv"))
 b4 = [d.chebyshev(row) for row in d.rows]
--- script-generator.py
+++ script-generator.py
@@ -1,12 +1,21 @@
 import sys, os
+from ezr import DATA, csv
 
-def printscript(dataset):
+def printscript(dataset, length):
   cwd = os.getcwd()
-  print(f'python3.13 {cwd}/extend.py {cwd}/{dataset} | tee {cwd}/{str.replace(dataset, "data/optimize", "exp/res")} &')
+  if length == "low":
+    print(f'python3.13 {cwd}/extend.py {cwd}/{dataset} | tee {cwd}/exp/res/low-dimension/{dataset.split("/")[-1]} &')
+  else:
+    print(f'python3.13 {cwd}/extend.py {cwd}/{dataset} | tee {cwd}/exp/res/high-dimension/{dataset.split("/")[-1]} &')
+
+print(f"mkdir -p {os.getcwd()}/exp/res")
+print(f"mkdir -p {os.getcwd()}/exp/res/low-dimension")
+print(f"mkdir -p {os.getcwd()}/exp/res/high-dimension")
+print(f"rm {os.getcwd()}/exp/res/low-dimension/*")
+print(f"rm {os.getcwd()}/exp/res/high-dimension/*")
 
 for i in os.listdir(f"data/optimize"):
   for j in os.listdir(f"data/optimize/{i}/"):
     if j[-4:] == ".csv":
-      printscript(str("data/optimize/"+i+"/"+j))
+      printscript(str("data/optimize/"+i+"/"+j), ("low" if len(DATA().adds(csv("data/optimize/"+i+"/"+j)).cols.x) < 6 else "high") )
   
-[printscript(arg) for arg in sys.argv if arg[-4:] == ".csv"]
