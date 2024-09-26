9bfa6bf174340544560de07adf5f7930cddab966
amirali <amirali@DESKTOP-2P0IDPU.localdomain>
2024-09-26 15:01:35 -0400

    HAndling a bug and removing trash

--- extend.py
+++ extend.py
@@ -25,41 +25,6 @@ for N in (20,30,40,50):
   the.Last = N
   smart = [d.shuffle().activeLearning() for _ in range(repeats)]
   smart = [d.chebyshev( lst[0] ) for lst in smart]
-  (low_dimensions_somes if len(d.cols.x) else high_dimensions_somes).append(stats.SOME(smart,f"smart,{N}"))
+  (low_dimensions_somes if len(d.cols.x) <6 else high_dimensions_somes).append(stats.SOME(smart,f"smart,{N}"))
 
 (stats.report(low_dimensions_somes, 0.01) if len(d.cols.x) < 6 else stats.report(high_dimensions_somes, 0.01))
-
-#-------------------------------------------------------------------------------
-
-'''
-d = DATA().adds(csv("data/optimize/misc/auto93.csv"))
-b4 = [d.chebyshev(row) for row in d.rows]
-somes = [stats.SOME(b4,f"asIs,{len(d.rows)}")]
-print(len(b4))
-
-rnd = lambda z: z
-scoring_policies = [
-  ('exploit', lambda B, R,: B - R),
-  ('explore', lambda B, R :  (exp(B) + exp(R))/ (1E-30 + abs(exp(B) - exp(R))))]
-repeats = 1
-
-for what,how in scoring_policies:
-  for the.Last in [0,20, 30, 40]:
-    for the.branch in [False, True]:
-      start = time.time()
-      result = []
-      runs = 0
-      for _ in range(repeats):
-         tmp=d.shuffle().activeLearning(score=how)
-         runs += len(tmp)
-         result += [rnd(d.chebyshev(tmp[0]))]
-
-      pre=f"{what}/b={the.branch}" if the.Last >0 else "rrp"
-      tag = f"{pre},{int(runs/repeats)}"
-      print(tag, f": {(time.time() - start) /repeats:.2f} secs")
-      somes +=   [stats.SOME(result,    tag)]
-      print((result))
-      input()
-
-stats.report(somes, 0.01)
-'''
