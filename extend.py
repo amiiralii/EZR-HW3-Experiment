0761f782599b9a8c0614868a7b90eca80aff838f
amirali <amirali@DESKTOP-2P0IDPU.localdomain>
2024-09-26 20:15:31 -0400

    experiment results

--- extend.py
+++ extend.py
@@ -1,6 +1,3 @@
-# on my machine, i ran this with:  
-#   python3.13 -B extend.py ../moot/optimize/[comp]*/*.csv
-
 import random, sys
 from ezr import the, DATA, csv
 import stats
@@ -9,22 +6,21 @@ dataset = sys.argv[1]
 
 d = DATA().adds(csv(dataset))
 b4 = [d.chebyshev(row) for row in d.rows]
-low_dimensions_somes = []
-high_dimensions_somes = []
-(low_dimensions_somes if len(d.cols.x) < 6 else high_dimensions_somes).append(stats.SOME(b4,f"asIs,{len(d.rows)}"))
+somes = []
+somes.append(stats.SOME(b4,f"asIs,{len(d.rows)}"))
 
-repeats = 1
+repeats = 20
 for N in (20,30,40,50):
   the.Last = N
   d = DATA().adds(csv(dataset))
   dumb = [d.clone(random.choices(d.rows, k=N)).chebyshevs().rows for _ in range(repeats)]
   dumb = [d.chebyshev( lst[0] ) for lst in dumb]
 
-  (low_dimensions_somes if len(d.cols.x) < 6 else high_dimensions_somes).append(stats.SOME(dumb,f"dumb,{N}"))
+  somes.append(stats.SOME(dumb,f"dumb,{N}"))
 
   the.Last = N
   smart = [d.shuffle().activeLearning() for _ in range(repeats)]
   smart = [d.chebyshev( lst[0] ) for lst in smart]
-  (low_dimensions_somes if len(d.cols.x) <6 else high_dimensions_somes).append(stats.SOME(smart,f"smart,{N}"))
+  somes.append(stats.SOME(smart,f"smart,{N}"))
 
-(stats.report(low_dimensions_somes, 0.01) if len(d.cols.x) < 6 else stats.report(high_dimensions_somes, 0.01))
+stats.report(somes, 0.01)
